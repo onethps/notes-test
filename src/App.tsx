96bdbe2fc2f939db129158f7e0fbfad96fcba4f8
onethps <onethps@gmail.com>
2023-05-06 03:57:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-red-500">TEXT</div>
+    </>
+  );
+}
+
+export default App;
